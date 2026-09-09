772cb9eb4df0b0371ff5723f9e5e18d06c3a3334
Mustafa Khaled <mustafakh565@gmail.com>
2026-09-09 13:41:37 +0300

    Question 01

---
+++ CSharpBasics05/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasics05
+{
+    internal class Book
+    {
+        private string password = "secret";
+    }
+}
--- CSharpBasics05/Program.cs
+++ CSharpBasics05/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 01
+            // 1. Question 01
+            // Answer:
+            // We cannot access the password field from Main because it is private.
+            // A private field can only be accessed from within the same class.
+            // Trying to access it from Main will cause a compilation error.
+            Book book = new Book();
+            //Console.WriteLine(book.password); // Error
+            #endregion
+
         }
     }
 }
